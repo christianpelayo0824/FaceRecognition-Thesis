83ac1ffa292fb70c81aa8e6aa32665bcb5df31c0
Your Name <you@example.com>
2018-11-10 01:43:24 +0800

    Add code for TestCamera.py

--- TestCamera.py
+++ TestCamera.py
@@ -1,1 +1,17 @@
-print("Hello")
+import cv2
+
+capture = cv2.VideoCapture(0)
+
+while(True):
+    ret, frame = capture.read()
+
+    grayscale = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+    cv2.imshow('Main Frame', frame)
+    cv2.imshow('GrayScale', grayscale)
+
+    if(cv2.waitKeyEx(20) & 0xFF == ord('q')):
+        break
+
+capture.release()
+cv2.destroyAllWindows()
